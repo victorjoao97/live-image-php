6461632ca8d9a33ed21b641905f705c2ca72aafb
João Victor Nascimento <victorjoao97@outlook.com>
2019-06-04 12:13:31 -0300

    add download event

--- download.php
+++ download.php
@@ -1,33 +1,41 @@
 <?
 require_once "config/config.php";
 $event = DBRead( 'events', "where id = '{$_GET['id']}' order by date desc limit 1" )[0];
-// Get real path for our folder
-$rootPath = realpath(path("assets/images/events/" . $event['id']));
-var_dump($rootPath);
-// Initialize archive object
-$zip = new ZipArchive();
-$zip->open($event['id'].'.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+if (!$event){
 
-// Create recursive directory iterator
-/** @var SplFileInfo[] $files */
-$files = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator($rootPath),
-    RecursiveIteratorIterator::LEAVES_ONLY
-);
-
-foreach ($files as $name => $file)
-{
-    // Skip directories (they would be added automatically)
-    if (!$file->isDir())
-    {
-        // Get real and relative path for current file
-        $filePath = $file->getRealPath();
-        $relativePath = substr($filePath, strlen($rootPath) + 1);
-
-        // Add current file to archive
-        $zip->addFile($filePath, $relativePath);
-    }
 }
+else
+{
+	// Get real path for our folder
+	$rootPath = realpath(path("assets/images/events/" . $event['id']));
+	var_dump($rootPath);
+	// Initialize archive object
+	$zip = new ZipArchive();
+	$zip->open($event['id'].'.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
 
-// Zip archive will be created only after closing object
-$zip->close();
+	// Create recursive directory iterator
+	/** @var SplFileInfo[] $files */
+	$files = new RecursiveIteratorIterator(
+	    new RecursiveDirectoryIterator($rootPath),
+	    RecursiveIteratorIterator::LEAVES_ONLY
+	);
+
+	foreach ($files as $name => $file)
+	{
+	    // Skip directories (they would be added automatically)
+	    if (!$file->isDir())
+	    {
+	        // Get real and relative path for current file
+	        $filePath = $file->getRealPath();
+	        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+	        // Add current file to archive
+	        $zip->addFile($filePath, $relativePath);
+	    }
+	}
+
+	// Zip archive will be created only after closing object
+	$zip->close();
+	header("location: ". $event['id']."zip");
+	header("location: events");
+}
